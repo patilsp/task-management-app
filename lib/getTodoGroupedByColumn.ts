c9abc6aead972846f6f6a33eb9e0ab3a8f3dd7e3
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-06-16 15:30:32 +0530

    database setup

--- lib/getTodoGroupedByColumn.ts
+++ lib/getTodoGroupedByColumn.ts
@@ -5,5 +5,34 @@ export const getTodoGroupedByColumn = async () => {
         process.env.NEXT_PUBLIC_DATABASE_ID!,
         process.env.NEXT_PUBLIC_TODO_COLLECTION_ID!
     );
+
     console.log(data);
-}
+
+    const todos = data.documents;
+
+    const columns = todos.reduce((acc, todo) => {
+            if(!acc.get(todo.status)){
+                acc.set(todo.status, {
+                    id: todo.status,
+                    todos: []
+                })
+            }
+            acc.get(todo.status)!.todos.push({
+                $id: todo.$id,
+                $createdAt: todo.$createdAt,
+                title: todo.title,
+                status:todo.status,
+
+                // get the image if it exists
+                ...(todo.image && { image: todo.image })
+
+            })
+
+            return acc;
+
+
+    }, new Map<TypedColmn, Column>)
+
+    console.log(columns);
+
+};
